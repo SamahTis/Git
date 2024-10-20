7c7f0fd2368be9ee144f9ada6a360f0783d70913
samah <tiouassiouinesamah@gmail.com>
2024-10-20 16:40:24 +0100

    class Morpion

---
+++ Morpion.py
@@ -0,0 +1,17 @@
+class Morpion:
+    def __init__(self):
+        self.plateau = [[' ' for _ in range(3)]
+                        for _ in range(3)]  # plateau de 3x3
+        self.joueur = 'X'  # Joueur par défaut
+
+    def afficher(self):
+        pass
+
+    def jouer_coup(self, ligne, colonne):
+        pass
+
+    def changer_joueur(self):
+        pass
+
+    def jouer(self):
+        pass
